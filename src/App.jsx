55cf52e9d1eeffe5688bf024ac5a228f26dcc5ff
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-09 23:08:22 +0530

    added card element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
+import Card from "./Card.jsx";
 
 function App() {
     return(
         <>
             <Header />
+            <Card />
             <Food />
             <Footer />
         </>
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" alt="Card image" src="https://via.placeholder.com/200"></img>
+            <h2 className="card-title">Demo app card</h2>
+            <p className="card-text">This is a card of the demo app</p>
+        </div>
+    );
+}
+export default Card
+
+// 31:55
