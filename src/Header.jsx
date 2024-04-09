05f6fdabe16f063c88df91c4d0032822fb91fdfe
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-09 22:45:31 +0530

    added header

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,15 @@ function Header(){
     return(
         <header>
             <h1>Demo Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
         </header>
     );
 }
