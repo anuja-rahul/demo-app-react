200431ac256517adc94907d49e2fd2588972af29
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-09 22:45:14 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
 function App() {
     return(
-        <Header></Header>
+        <>
+            <Header />
+            <Food />
+            <Footer />
+        </>
     );
 }
 
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Demo app</p>
+        </footer>
+    );
+}
+
+export default Footer
